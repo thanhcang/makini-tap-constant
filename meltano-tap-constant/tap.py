2ea91637089821967388394b28e6ffab4d33fbe4
Vo Thanh Cang <vtcanglt@gmail.com>
2024-11-18 14:16:46 +0700

    Added : tap constant

--- meltano-tap-constant/tap.py
+++ meltano-tap-constant/tap.py
@@ -9,17 +9,9 @@ class TapRestConstant(Tap):
 
     name = "tap-rest-constant"
 
-    # Define the properties for the tap from environment variables
-    stream_maps = [
-        {
-            "name": "work_orders_statuses",
-            "data": ["new", "open", "onhold"]
-        },
-        {
-            "name": "work_orders_statuses-2",
-            "data": ["new", "open", "onhold"]
-        },
-    ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stream_maps = self.load_data_from_env()
 
     # Define the stream schema
     @property
@@ -43,6 +35,7 @@ class TapRestConstant(Tap):
     def discover(self):
         """Discover the streams. This is typically where metadata is discovered."""
         for stream in self.stream_maps:
+            self.logger.info(f"Discovering stream: {stream['name']}")
             # Here we return the stream metadata in a way Singer expects
             yield {
                 "stream": stream["name"],
@@ -54,6 +47,7 @@ class TapRestConstant(Tap):
     def sync(self):
         """Sync the streams and produce the records."""
         for stream in self.stream_maps:
+            self.logger.info(f"Syncing stream: {stream['name']}")
             # Creating record per stream
             record = {
                 "name": stream["name"],
@@ -72,9 +66,23 @@ class TapRestConstant(Tap):
 
     # Helper method to load data from environment
     def load_data_from_env(self):
+        """Load stream data from the environment variable TAP_REST_API_MSDK_STREAMS."""
         stream_data = os.getenv("TAP_REST_API_MSDK_STREAMS", "[]")
         try:
             # Load stream data from the environment variable
+            self.logger.info("Loading stream data from environment variable.")
             return json.loads(stream_data)
-        except json.JSONDecodeError:
-            return self.stream_maps
+        except json.JSONDecodeError as e:
+            # Log error if there's an issue with parsing the JSON
+            self.logger.error(f"Failed to load stream data from environment. Error: {e}")
+            # If there's an error loading, return default stream maps
+            return [
+                {
+                    "name": "work_orders_statuses",
+                    "data": ["new", "open", "onhold"]
+                },
+                {
+                    "name": "work_orders_statuses-2",
+                    "data": ["new", "open", "onhold"]
+                },
+            ]
